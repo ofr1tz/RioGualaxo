891570699da00b3ff777dd3087f8bbacd73e8c85
ofr1tz <ofr1tz@yahoo.de>
2018-01-16 21:33:17 +0000

    remove setwd, print

--- RioGualaxoNDVIhist.R
+++ RioGualaxoNDVIhist.R
@@ -1,6 +1,3 @@
-# set working directory
-setwd("D:/OneDrive/Code/R/R GIS/Rio Gualaxo")
-
 # Install tidyverse package if necessary
 if (!require(tidyverse)) {
       install.packages("tidyverse")
@@ -19,6 +16,13 @@ aug17 <- aug17 %>% mutate(Scene=ymd("2017-08-29")) %>% gather(-Scene, key="Posit
 dat <- bind_rows(oct15, nov15, aug17)
 
 # Plot histogram
-ggplot(dat, aes(NDVI))+geom_histogram(binwidth=.005, fill="darkgreen")+facet_grid(Scene~.)+coord_cartesian(xlim=c(0,1))+theme_bw()
+p <- ggplot(dat, aes(NDVI))+
+      geom_histogram(binwidth=.005, col="darkgreen",fill="darkgreen")+
+      facet_grid(Scene~.)+
+      coord_cartesian(xlim=c(0,1))+
+      theme_bw()
 
-write.table(dat, "dat.txt")
+print(p)
+
+# Write data
+# write.table(dat, "dat.txt")
